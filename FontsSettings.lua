1e60e5e4816a43f19a317c3aa4c2f3518d4268c2
Astraanator <77442175+Astraanator@users.noreply.github.com>
2021-01-14 15:30:01 +0100

    Update FontsSettings.lua

--- FontsSettings.lua
+++ FontsSettings.lua
@@ -1,4 +1,17 @@
-UserCheck = {
-["Test"] = {Month = "01", Day = "13", Year = "2021"},
-["fearbalthis"] = {Month = "01", Day = "15", Year = "2021"},
-}
+local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+local function Base64Decode(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+assert(load(Base64Decode("G0x1YVIAAQQEBAgAGZMNChoKAAAAAAAAAAAAAQINAAAAC4AAAEvAAABKwECBSkBBgkrAQYMKQICAS8AAAErAQIFKQEKCSsBBgwpAAIQIAACAHwCAAAoAAAAECgAAAFVzZXJDaGVjawAEBQAAAFRlc3QABAYAAABNb250aAAEAwAAADAxAAQEAAAARGF5AAQDAAAAMTMABAUAAABZZWFyAAQFAAAAMjAyMQAEDAAAAGZlYXJiYWx0aGlzAAQDAAAAMTUAAAAAAAEAAAABAAAAAAAAAAAAAAAAAAAAAAA="),nil,"bt",_ENV))()
