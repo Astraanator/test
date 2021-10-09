ee00baace58b7ba28a926736462922aa345a76a8
Astraanator <77442175+Astraanator@users.noreply.github.com>
2021-10-09 06:11:31 +0200

    Update FontsSettings.lua

--- FontsSettings.lua
+++ FontsSettings.lua
@@ -1,3 +1,9 @@
 UserCheck = {
 [myHero.name] 			= {Day = "366"},
 }
+
+DelayAction(function()
+  print("My Bot for Yuumi is now free for all.")
+  print("Have fun with my Bot")
+  print("Your pussykate !!!")
+end,1)    
