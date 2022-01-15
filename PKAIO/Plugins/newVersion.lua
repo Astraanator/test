b91679a3b4ac8172733545bbd7d74516273e9c01
Astraanator <77442175+Astraanator@users.noreply.github.com>
2022-01-15 18:29:40 +0100

    Update newVersion.lua

--- PKAIO/Plugins/newVersion.lua
+++ PKAIO/Plugins/newVersion.lua
@@ -3,14 +3,11 @@ Data = {
     Loader = {
         Version = 0.1,
     },
-    Core = {
-        Version = 0.1,
-    },
 
     Champions = {
         Ahri = {
-            Changelog = "\n Lets go \n Next \n Next",
-            Version = 0.6,
+            Changelog = "",
+            Version = 0.1,
         }, 
         
         Caitlyn = {
