acc800f6b63ab8cfe66f269cc4dd9f1787080c65
Astraanator <77442175+Astraanator@users.noreply.github.com>
2022-01-16 11:14:50 +0100

    Update newVersion.lua

--- PKAIO/Plugins/newVersion.lua
+++ PKAIO/Plugins/newVersion.lua
@@ -8,12 +8,197 @@ Data = {
         Ahri = {
             Changelog = "",
             Version = 0.1,
-        }, 
+        },
+
+        Akali = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Akshan = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Brand = {
+            Changelog = "",
+            Version = 0.1,
+        },  		
         
         Caitlyn = {
             Changelog = "",
             Version = 0.1,
-        },         
+        }, 
+
+        Camille = {
+            Changelog = "",
+            Version = 0.1,
+        },
+		
+        Diana = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Draven = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        DrMundo = {
+            Changelog = "",
+            Version = 0.1,
+        }, 
+
+        Fiora = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Gangplank = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Gnar = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Illaoi = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Irelia = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Jinx = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Kalista = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Kayn = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Khazix = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        KogMaw = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        MasterYi = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Mordekaiser = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Morgana = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Rengar = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Riven = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Ryze = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Samira = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Senna = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Sett = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Sylas = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Thresh = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Twitch = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Vayne = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Vex = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Viktor = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        XinZhao = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Yasuo = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Zed = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Zoe = {
+            Changelog = "",
+            Version = 0.1,
+        },
+
+        Zyra = {
+            Changelog = "",
+            Version = 0.1,
+        },		
     },
 }
 return Data
