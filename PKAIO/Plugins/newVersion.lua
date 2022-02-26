8ae8ec42c6d21f9095eab204ab889ce7de8cb3c4
Astraanator <77442175+Astraanator@users.noreply.github.com>
2022-02-26 07:34:46 +0100

    Update newVersion.lua

--- PKAIO/Plugins/newVersion.lua
+++ PKAIO/Plugins/newVersion.lua
@@ -1,7 +1,7 @@
 Data = {
 
     Loader = {
-        Version = 0.6,
+        Version = 0.7,
     },
 
     Champions = {
@@ -127,7 +127,7 @@ Data = {
 		
         Renata = {
             Changelog = "",
-            Version = 0.4,
+            Version = 0.5,
         },		
 
         Riven = {
